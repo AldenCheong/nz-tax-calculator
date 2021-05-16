48ae357941a98a8e37ba3101fdbc41efb9924eda
Alden <gze.xie@gmail.com>
2021-05-16 22:14:22 +1200

    Add text field

--- src/App.js
+++ src/App.js
@@ -1,14 +1,27 @@
 import logo from "./logo.svg";
 import "./App.css";
+import TextField from "@material-ui/core/TextField";
+import { ThemeProvider, createMuiTheme } from "@material-ui/core/styles";
+
+const theme = createMuiTheme({
+  palette: {
+    text: {
+      primary: "#fff",
+    }
+  },
+});
 
 function App() {
 	return (
-		<div className="App">
-			<header className="App-header">
-				<img src={logo} className="App-logo" alt="logo" />
-				<p>Tax Calculator</p>
-			</header>
-		</div>
+    <ThemeProvider theme={theme}>
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>Tax Calculator</p>
+          <TextField variant="filled" color="secondary" label="Annual Income" />
+        </header>
+      </div>
+    </ThemeProvider>
 	);
 }
 
