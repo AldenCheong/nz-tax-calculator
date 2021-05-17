d0aff5460090b321a3febf582ba2ce30d007fd2f
Alden <gze.xie@gmail.com>
2021-05-17 21:09:27 +1200

    Add tax bracket image and after tax label

--- src/App.js
+++ src/App.js
@@ -1,27 +1,42 @@
+import { useState } from "react";
 import logo from "./logo.svg";
+import TaxBracket2021 from "./images/TaxBracket-1stApril2021.png";
 import "./App.css";
 import TextField from "@material-ui/core/TextField";
 import { ThemeProvider, createMuiTheme } from "@material-ui/core/styles";
 
 const theme = createMuiTheme({
-  palette: {
-    text: {
-      primary: "#fff",
-    }
-  },
+	palette: {
+		text: {
+			primary: "#fff",
+		},
+	},
 });
 
 function App() {
+	const [afterTaxAmount, setAfterTaxAmount] = useState(null);
+
+	const calcAfterTaxAmount = (event) => {
+		setAfterTaxAmount(Number(event.target.value) / 2);
+	};
+
 	return (
-    <ThemeProvider theme={theme}>
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>Tax Calculator</p>
-          <TextField variant="filled" color="secondary" label="Annual Income" />
-        </header>
-      </div>
-    </ThemeProvider>
+		<ThemeProvider theme={theme}>
+			<div className="App">
+				<header className="App-header">
+					<img src={logo} className="App-logo" alt="logo" />
+					<p>Tax Calculator</p>
+					<TextField
+						variant="filled"
+						color="secondary"
+						label="Annual Income"
+						onChange={calcAfterTaxAmount}
+					/>
+					<p>After tax: {afterTaxAmount}</p>
+					<img src={TaxBracket2021} alt="tax bracket" />
+				</header>
+			</div>
+		</ThemeProvider>
 	);
 }
 
