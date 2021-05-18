669fe71ff0850ff2c5981e53cefaf7ac6e944244
Alden <gze.xie@gmail.com>
2021-05-18 22:17:28 +1200

    Restructure textfield onChange method to be more readable

--- src/App.js
+++ src/App.js
@@ -27,28 +27,35 @@ function App() {
     getTaxBrackets();
   }, [])
 
-	const calcTaxAmount = (event) => {
-		const initialAmount = Number(event.target.value);
-    if (initialAmount <= 0) return;
+	const calculateDetails = (event) => {
+    const initialAmount = Number(event.target.value);
+    
+    const calculateTax = () => {
+      if (initialAmount <= 0) return;
 
-    let remainder = initialAmount;
-    let taxAmount = 0;
-    for (let i = 0; i < taxBrackets.length; i++) {
-      const {threshold, rate} = taxBrackets[i];
-      const previousThreshold = i > 0 && taxBrackets[i - 1].threshold;
-      
-      if (initialAmount <= threshold || i === (taxBrackets.length - 1)) {
-        if (previousThreshold) remainder -= previousThreshold;
-        taxAmount += remainder * (rate / 100);
-        break;
+      let remainder = initialAmount;
+      let taxAmount = 0;
+      for (let i = 0; i < taxBrackets.length; i++) {
+        const {threshold, rate} = taxBrackets[i];
+        const previousThreshold = i > 0 && taxBrackets[i - 1].threshold;
+        
+        if (initialAmount <= threshold || i === (taxBrackets.length - 1)) {
+          if (previousThreshold) remainder -= previousThreshold;
+          taxAmount += remainder * (rate / 100);
+          break;
+        }
+
+        const currentBracket = previousThreshold ? threshold - previousThreshold : threshold;
+        taxAmount += currentBracket * (rate / 100);
       }
 
-      const currentBracket = previousThreshold ? threshold - previousThreshold : threshold;
-      taxAmount += currentBracket * (rate / 100);
+      setTaxAmount(taxAmount.toFixed(2));
     }
+    
+    const calculateAcc = () => setAccAmount(initialAmount * (1.39/100));
 
-		setTaxAmount(taxAmount.toFixed(2));
-    setAccAmount(initialAmount * (1.39/100));
+    calculateTax();
+    calculateAcc();
 	};
 
 	return (
@@ -61,7 +68,7 @@ function App() {
 						variant="filled"
 						color="secondary"
 						label="Annual Income"
-						onChange={calcTaxAmount}
+						onChange={calculateDetails}
 					/>
 					<p>Tax Amount: {taxAmount}</p>
 					<p>ACC Amount: {accAmount}</p>
