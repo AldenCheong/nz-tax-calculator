efbf144442b0b16e9b56178542a582f5bd0048a8
Alden <gze.xie@gmail.com>
2021-05-21 21:40:13 +1200

    Add options for KiwiSaver

--- src/App.js
+++ src/App.js
@@ -98,9 +98,14 @@ function App() {
             <MenuItem value="">
               <em>Opt out</em>
             </MenuItem>
-            <MenuItem value={10}>Ten</MenuItem>
-            <MenuItem value={20}>Twenty</MenuItem>
-            <MenuItem value={30}>Thirty</MenuItem>
+            <MenuItem value={3}>3%</MenuItem>
+            <MenuItem value={4}>4%</MenuItem>
+            <MenuItem value={6}>6%</MenuItem>
+            <MenuItem value={8}>8%</MenuItem>
+            <MenuItem value={10}>10%</MenuItem>
+            <MenuItem value="Custom">
+              <em>Custom</em>
+            </MenuItem>
           </Select>
         </FormControl>
 				<p>Take home pay: {takeHomePay}</p>
