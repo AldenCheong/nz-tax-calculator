62fef0879ac90e1c3f335968efd521aaaf2b30d6
Alden <gze.xie@gmail.com>
2021-05-30 16:31:26 +1200

    Trigger calculation based on inserted input when frequency changes

--- src/App.js
+++ src/App.js
@@ -10,7 +10,8 @@ import IncomeFrequency from "./helpers/IncomeFrequencyEnum";
 import "./App.css";
 
 function App() {
-	const [income, setIncome] = useState(0);
+	const [incomeInput, setIncomeInput] = useState(0);
+	const [annualIncome, setAnnualIncome] = useState(0);
 	const [incomeFrequency, setIncomeFrequency] = useState(IncomeFrequency.annually);
 	const [deductable, setDeductable] = useState({ tax: 0, acc: 0, kiwi: 0 });
 	const [bracket, setBracket] = useState({ tax: [], acc: [] });
@@ -44,16 +45,30 @@ function App() {
 
 	useEffect(() => {
 		const { tax, acc, kiwi } = deductable;
-		setTakeHomePay(income - tax - acc - kiwi);
-	}, [deductable, income]);
+		setTakeHomePay(annualIncome - tax - acc - kiwi);
+	}, [deductable, annualIncome]);
   
   useEffect(() => {
-    income >= 0 && setDeductable(calculateDeductables(income, bracket, kiwiSaver));
-  }, [income, bracket, kiwiSaver])
+    annualIncome >= 0 && setDeductable(calculateDeductables(annualIncome, bracket, kiwiSaver));
+  }, [annualIncome, bracket, kiwiSaver])
 
-	const calculateDetails = (event) => setIncome(Number(event.target.value));
+  const convertIncomeToAnnually = (income, frequency) => {
+    if (frequency === IncomeFrequency.hourly) income *= (52 * 40);
+    if (frequency === IncomeFrequency.weekly) income *= 52;
+    if (frequency === IncomeFrequency.monthly) income *= 12;
+    return income;
+  }
+	const calculateIncome = (event) => {
+    const inputValue = Number(event.target.value);
+    setIncomeInput(inputValue);
+    setAnnualIncome(convertIncomeToAnnually(inputValue, incomeFrequency));
+  }
   const onToggleKiwiSaver = () => setKiwiSaver({ ...kiwiSaver, include: !kiwiSaver.include });
-  const onSelectFrequency = (event) => setIncomeFrequency(event.target.value); 
+  const onSelectFrequency = (event) => {
+    const frequency = event.target.value;
+    setIncomeFrequency(frequency); 
+    setAnnualIncome(convertIncomeToAnnually(incomeInput, frequency));
+  }
   const setKiwiSaverRate = (rate) => setKiwiSaver({ ...kiwiSaver, rate: rate });
   const setIncomeFrequencyOptions = () => {
     return Object.values(IncomeFrequency).map((frequency) => (
@@ -70,7 +85,7 @@ function App() {
   }
 
   const rows = [
-    { id: 0, variable: "Income", ...getValues(income) },
+    { id: 0, variable: "Income", ...getValues(annualIncome) },
     { id: 1, variable: "Tax", ...getValues(deductable.tax) },
     { id: 2, variable: "Acc", ...getValues(deductable.acc) },
     { id: 3, variable: "KiwiSaver", ...getValues(deductable.kiwi) },
@@ -94,7 +109,7 @@ function App() {
 					size="small"
 					color="secondary"
 					label="Income"
-					onChange={calculateDetails}
+					onChange={calculateIncome}
 				/>
         <FormControl variant="outlined" size="small">
           <Select value={incomeFrequency} onChange={onSelectFrequency}>
