2d063a08965df95673a38e342aabd29948dde4ea
Alden <gze.xie@gmail.com>
2021-06-07 20:09:25 +1200

    Utilize react-chartjs-2 for DetailDonutChart

--- src/components/DetailDonutChart.jsx
+++ src/components/DetailDonutChart.jsx
@@ -1,39 +1,26 @@
-import { useEffect, useRef } from "react";
-import * as ChartJs from "chart.js";
-
-ChartJs.Chart.register.apply(
-  null,
-  Object.values(ChartJs).filter(chartClass => chartClass.id),
-)
+import { Doughnut } from 'react-chartjs-2';
 
 const DetailDonutChart = () => {
-  const canvasRef = useRef(null);
-
-  useEffect(() => {
-    new ChartJs.Chart(canvasRef.current, {
-      type: "doughnut",
-      data: {
-        labels: [
-          'Negative',
-          'Neutral',
-          'Positive'
-        ],
-        datasets: [{
-          label: 'My First Dataset',
-          data: [300, 50, 100],
-          backgroundColor: [
-            'lightpink',
-            'lightskyblue',
-            'palegreen'
-          ],
-          hoverOffset: 15
-        }]
-      },
-    });
-  }, [])
+  const data = {
+    labels: [
+      'Negative',
+      'Neutral',
+      'Positive'
+    ],
+    datasets: [{
+      label: 'My First Dataset',
+      data: [300, 50, 100],
+      backgroundColor: [
+        'lightpink',
+        'lightskyblue',
+        'palegreen'
+      ],
+      hoverOffset: 15
+    }]
+  }
 
   return (
-    <canvas ref={canvasRef}></canvas>
+    <Doughnut data={data} />
   )
 }
 
