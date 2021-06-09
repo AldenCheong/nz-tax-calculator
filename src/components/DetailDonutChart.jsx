d73b592f25ee9bbedc4a53e5e796d257d4a662a4
Alden <gze.xie@gmail.com>
2021-06-09 21:25:11 +1200

    Formatted DetailDonutChart.jsx

--- src/components/DetailDonutChart.jsx
+++ src/components/DetailDonutChart.jsx
@@ -1,51 +1,45 @@
-import { Doughnut } from 'react-chartjs-2';
+import { Doughnut } from "react-chartjs-2";
 
 const styles = {
-  donutChart: {
-    width: 400,
-    height: 400,
-  },
-}
+	donutChart: {
+		width: 400,
+		height: 400,
+	},
+};
 
 const DetailDonutChart = () => {
-  const data = {
-    labels: [
-      'Negative',
-      'Neutral',
-      'Positive'
-    ],
-    datasets: [{
-      data: [300, 50, 100],
-      backgroundColor: [
-        'lightpink',
-        'lightskyblue',
-        'palegreen'
-      ],
-      hoverOffset: 5
-    }]
-  }
+	const data = {
+		labels: ["Negative", "Neutral", "Positive"],
+		datasets: [
+			{
+				data: [300, 50, 100],
+				backgroundColor: ["lightpink", "lightskyblue", "palegreen"],
+				hoverOffset: 5,
+			},
+		],
+	};
 
-  const options = {
-    plugins: {
-      legend: {
-        display: false,
-      }
-    },
-    layout: {
-      padding: {
-          left: 20,
-          right: 20,
-          top: 20,
-          bottom: 20
-      }
-    }
-  }
+	const options = {
+		plugins: {
+			legend: {
+				display: false,
+			},
+		},
+		layout: {
+			padding: {
+				left: 20,
+				right: 20,
+				top: 20,
+				bottom: 20,
+			},
+		},
+	};
 
-  return (
-    <div style={styles.donutChart}>
-      <Doughnut data={data} options={options} />
-    </div>
-  )
-}
+	return (
+		<div style={styles.donutChart}>
+			<Doughnut data={data} options={options} />
+		</div>
+	);
+};
 
-export default DetailDonutChart
+export default DetailDonutChart;
