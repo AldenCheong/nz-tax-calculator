4051661bde3bae639eb76ce8fece256d8f4945b8
Alden <gze.xie@gmail.com>
2021-06-18 22:18:30 +1200

    WIP - Updated material-ui/core and bunch of dependencies, but build is failing now

--- src/components/DetailDonutChart.jsx
+++ src/components/DetailDonutChart.jsx
@@ -1,26 +1,28 @@
 import { useState } from "react";
 import { Doughnut } from "react-chartjs-2";
-import { ToggleButtonGroup, ToggleButton } from "@material-ui/lab";
-import useStyles from "./DetailDonutChart.style.js"
+// import { ToggleButtonGroup, ToggleButton } from "@material-ui/core";
+import ToggleButton from "@material-ui/core/ToggleButton";
+import ToggleButtonGroup from "@material-ui/core/ToggleButtonGroup";
+import useStyles from "./DetailDonutChart.style.js";
 
 const DetailDonutChart = ({ chartData }) => {
-  const styles = useStyles();
+	const styles = useStyles();
 	let labels = [];
 	let annuallies = [];
 	let percentages = [];
 	let colors = [];
-  let grossPayData = {};
-  let takeHomePayData = {};
-  let taxData = {};
-  let deductable = 0;
-  const [displayFormat, setDisplayFormat] = useState('%');
-  
-  // Loop through and assign into variables
+	let grossPayData = {};
+	let takeHomePayData = {};
+	let taxData = {};
+	let deductable = 0;
+	const [displayFormat, setDisplayFormat] = useState("%");
+
+	// Loop through and assign into variables
 	chartData.forEach((row) => {
 		if (row.variable === "Gross Pay") {
-      grossPayData = row;
-      return;
-    }
+			grossPayData = row;
+			return;
+		}
 		labels.push(row.variable);
 		annuallies.push(row.annually);
 		percentages.push(row.percentage);
@@ -28,16 +30,16 @@ const DetailDonutChart = ({ chartData }) => {
 			row.variable === "Take Home Pay" ? "lightskyblue" : "lightpink"
 		);
 
-    if (row.variable === "Take Home Pay") {
-      takeHomePayData = row;
-      return;
-    }
+		if (row.variable === "Take Home Pay") {
+			takeHomePayData = row;
+			return;
+		}
 
-    if (row.variable === "Tax") {
-      taxData = row;
-    }
+		if (row.variable === "Tax") {
+			taxData = row;
+		}
 
-    deductable += Number(row.annually);
+		deductable += Number(row.annually);
 	});
 
 	const processedData = {
@@ -56,11 +58,14 @@ const DetailDonutChart = ({ chartData }) => {
 			legend: {
 				display: false,
 			},
-      tooltip: {
-        callbacks: {
-          label: (tooltipItem) => `${tooltipItem.label}: ${tooltipItem.raw} (${percentages[tooltipItem.dataIndex]})`,
-        },
-      },
+			tooltip: {
+				callbacks: {
+					label: (tooltipItem) =>
+						`${tooltipItem.label}: ${tooltipItem.raw} (${
+							percentages[tooltipItem.dataIndex]
+						})`,
+				},
+			},
 		},
 		layout: {
 			padding: {
@@ -72,52 +77,54 @@ const DetailDonutChart = ({ chartData }) => {
 		},
 	};
 
-  const handleDisplayFormat = () => {}
+	const handleDisplayFormat = (_value, newDisplayFormat) =>
+		setDisplayFormat(newDisplayFormat);
 
 	return (
-    <div className={styles.detailBlock}>
-      <div className={styles.donutChart}>
-        <Doughnut data={processedData} options={options} />
-        <div className={styles.chartInner}>
-          <div className={styles.chartStatus}>{grossPayData.variable}</div>
-          <div className={styles.chartValue}>{grossPayData.annually}</div>
-        </div>
-      </div>
-      <div className={styles.explanationPanel}>
-        <div className={styles.explanationRow}>
-          <div className={styles.explanationBlock}>
-            <ToggleButtonGroup
-              value={displayFormat}
-              onChange={handleDisplayFormat}
-              aria-label="text alignment"
-            >
-              <ToggleButton value="%" variant="outlined" color="secondary" aria-label="left aligned">
-                %
-              </ToggleButton>
-              <ToggleButton value="#" variant="outlined" color="secondary" aria-label="right aligned">
-                #
-              </ToggleButton>
-            </ToggleButtonGroup>
-          </div>
-        </div>
-        <div className={styles.explanationRow}>
-          <div className={styles.explanationBlock}>
-            <span>Your take home pay is </span>
-            <span className={styles.highlightText}>{takeHomePayData.annually}</span>
-          </div>
-        </div>
-        <div className={styles.explanationRow}>
-          <div className={styles.explanationBlock}>
-            <span>Deductables</span>
-            <span className={styles.highlightText}>{deductable.toFixed(2)}</span>
-          </div>
-          <div className={styles.explanationBlock}>
-            <span>Effective Tax</span>
-            <span className={styles.highlightText}>{taxData.annually}</span>
-          </div>
-        </div>
-      </div>
-    </div>
+		<div className={styles.detailBlock}>
+			<div className={styles.donutChart}>
+				<Doughnut data={processedData} options={options} />
+				<div className={styles.chartInner}>
+					<div className={styles.chartStatus}>{grossPayData.variable}</div>
+					<div className={styles.chartValue}>{grossPayData.annually}</div>
+				</div>
+			</div>
+			<div className={styles.explanationPanel}>
+				<div className={styles.explanationRow}>
+					<div className={styles.explanationBlock}>
+						<ToggleButtonGroup
+							color="primary"
+							value={displayFormat}
+							exclusive
+							onChange={handleDisplayFormat}
+						>
+							<ToggleButton value="%">%</ToggleButton>
+							<ToggleButton value="#">#</ToggleButton>
+						</ToggleButtonGroup>
+					</div>
+				</div>
+				<div className={styles.explanationRow}>
+					<div className={styles.explanationBlock}>
+						<span>Your take home pay is </span>
+						<span className={styles.highlightText}>
+							{takeHomePayData.annually}
+						</span>
+					</div>
+				</div>
+				<div className={styles.explanationRow}>
+					<div className={styles.explanationBlock}>
+						<span>Deductables</span>
+						<span className={styles.highlightText}>
+							{deductable.toFixed(2)}
+						</span>
+					</div>
+					<div className={styles.explanationBlock}>
+						<span>Effective Tax</span>
+						<span className={styles.highlightText}>{taxData.annually}</span>
+					</div>
+				</div>
+			</div>
+		</div>
 	);
 };
 
